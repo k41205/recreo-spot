38db8d86ef51a52c0f3c7929c34d544745b75b2b
k41205 <giacomocavarretta@gmail.com>
2024-03-17 16:20:37 +0000

    fix(url-api): change url for api client side

--- public/js/errors-csr.js
+++ public/js/errors-csr.js
@@ -14,7 +14,6 @@ export const runError = (errors) => {
     const errorSection = document.querySelector(".error");
     const errorOverlay = document.querySelector(".overlay");
     if (!errorSection.contains(e.target)) {
-      console.log("Clicked outside the div!");
       errorSection.classList.remove("active");
       errorOverlay.classList.remove("active");
     }
